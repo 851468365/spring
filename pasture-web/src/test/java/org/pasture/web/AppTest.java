2918334637b9e7af21918a3859bb03eb6a9a1d0a
851468365 <851468365@qq.com>
2017-07-23 07:38:50 +0800

    更新测试类

--- pasture-web/src/test/java/org/pasture/web/AppTest.java
+++ pasture-web/src/test/java/org/pasture/web/AppTest.java
@@ -3,13 +3,12 @@ package org.pasture.web;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.nxyhj.pasture.student.service.interfaces.IStudentSV;
 
@@ -22,10 +21,16 @@ public class AppTest{
 	
 	@Test
     public void test(){
-		HashMap hashMap = new HashMap();
-		hashMap.put("sno", "001");
-		List<Map> selectStudentScore = 	iStudentSV.selectStudentScore(hashMap);
-		PageHelper.startPage(1, 2);
-		System.out.println("selectStudentScore:"+selectStudentScore);
+        //分页参数
+        Page page = PageHelper.startPage(1, 3);
+        //查询参数
+        HashMap hashMap = new HashMap();
+        hashMap.put("sno", "001");
+        //查询结果
+        List<Map> selectStudentScore = iStudentSV.selectStudentScore(hashMap);
+        //遍历结果集
+        for (Map map : selectStudentScore) {
+            System.out.println(map);
+        }
     }
 }
